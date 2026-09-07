4a2df1b60d271709982ec6bc94fc213a074d3ffc
425003139 <425003139@ntc.edu.ph>
2026-09-07 18:50:22 +0800

    Add grade remarks

--- main.cpp
+++ main.cpp
@@ -26,6 +26,22 @@ int main()
     int averageGrade = (prelimGrade + midtermGrade + finalsGrade)/3.0;
     cout << "\n Average: " << (averageGrade) << endl;
 
+    if  (averageGrade >= 95 && averageGrade <= 100){
+        cout << "Remarks: Outstanding" << endl;
+    }
+    else if (averageGrade >= 90){
+        cout << "Remarks: Very Good" << endl;
+    }
+    else if (averageGrade >= 85){
+        cout << "Remarks: Good" << endl;
+    }
+    else if (averageGrade >= 75){
+        cout << "Remarks: Passed" << endl;
+    }
+    else if (averageGrade >= 0){
+        cout << "Failed" << endl;
+    }
+
     cin.get();
     return (0);
 }
